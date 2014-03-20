a07ecbe9f19f6c59a12d25e9b4ae05313fc5831f
karrakis <karrakis@gmail.com>
2014-03-19 19:48:59 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fenrir::Application.config.secret_key_base = 'd60403fb14d48c8037bddf8614d74085a6f0f089a33704baa0629fc4735c4d924b08cb04382ca1e240c52243fb4a5c40ac0b3f18f9ad31ee694c2ac3b310ca77'
