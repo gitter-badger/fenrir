6f8149f086cbe9e4435f77224a79f6465a3c4c60
karrakis <karrakis@gmail.com>
2014-07-16 16:40:51 -0500

    hurp durp

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   def serve
-    path = "#{Rails.root.to_s}/app/assets/images/#{params[:filename]}"
+    path = "#{Rails.root.to_s}/app/assets/images/#{params[:filename]}.jpg"
 
     send_file( path,
       :disposition => 'inline',
